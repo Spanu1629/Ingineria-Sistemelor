30777cf22645e78912a895dec7de4a5fe6364ce0
Spanu <spanujunior@gmail.com>
2026-03-11 15:06:25 +0200

    Lab2 ex2

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Student {
     private int numarMatricol;
     private String nume;
@@ -13,4 +15,29 @@ public class Student {
     public String toString() {
         return "Numar Matricol: "+numarMatricol+", Nume: "+nume+", Prenume: "+prenume+", Formatie de Studiu: "+formatieDeStudiu;
     }
+    public boolean existaInLista(List<Student> lista) {
+        for (Student st : lista) {
+            if (st.numarMatricol==this.numarMatricol&&
+                    st.nume.equals(this.nume) &&
+                    st.prenume.equals(this.prenume) &&
+                    st.formatieDeStudiu.equals(this.formatieDeStudiu)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+void main(){
+    Student s1=new Student(120, "Popa", "Alis", "TI21/2");
+    Student s2=new Student(112, "Popa", "Maria", "TI21/1");
+    Student s3=new Student(125,"Ionescu","Paul","ISM 21/1");
+    List<Student> listaStudenti = new ArrayList<>();
+    listaStudenti.add(s1);
+    listaStudenti.add(s2);
+    for (Student s : listaStudenti) {
+        System.out.println(s);
+    }
+    System.out.println("Studentul Alis exista? " + s1.existaInLista(listaStudenti));
+    System.out.println("Studentul Maria exista? " + s2.existaInLista(listaStudenti));
+    System.out.println("Studentul Paul exista? " + s3.existaInLista(listaStudenti));
 }
