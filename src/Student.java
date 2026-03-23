0af08a621fddf7d0422475f041dac4056a724140
Spanu <spanujunior@gmail.com>
2026-03-23 08:34:20 +0200

    Lab3 Ex2

--- src/Student.java
+++ src/Student.java
@@ -12,6 +12,21 @@ public class Student {
         prenume=pre;
         formatieDeStudiu=grupa;
     }
+    public int getNrMatricol() {
+        return numarMatricol;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getFormatieDeStudiu() {
+        return formatieDeStudiu;
+    }
     public String toString() {
         return "Numar Matricol: "+numarMatricol+", Nume: "+nume+", Prenume: "+prenume+", Formatie de Studiu: "+formatieDeStudiu;
     }
@@ -39,7 +54,7 @@ public class Student {
     }
 }
 void main(){
-    Student s1=new Student(120, "Popa", "Alis", "TI21/2");
+    /*Student s1=new Student(120, "Popa", "Alis", "TI21/2");
     Student s2=new Student(112, "Popa", "Maria", "TI21/1");
     Student s3=new Student(125,"Popescu","Paul","ISM 21/1");
     List<Student> listaStudenti = new ArrayList<>();
@@ -51,5 +66,47 @@ void main(){
     Set<Student> setStudenti = new HashSet<>(listaStudenti);
     System.out.println("Exista studentul Alis? "+setStudenti.contains(s1));
     System.out.println("Exista studentul Maria? "+setStudenti.contains(s2));
-    System.out.println("Exista studentul Paul? "+setStudenti.contains(s3));
+    System.out.println("Exista studentul Paul? "+setStudenti.contains(s3));*/
+    try {
+        List<String> linii = Files.readAllLines(Paths.get("src/studenti_in.txt"));
+
+        List<Student> studenti = new ArrayList<>();
+
+        // citim fiecare linie si folosim split(",")
+        for (String linie : linii) {
+            String[] campuri = linie.split(",");
+
+            int id = Integer.parseInt(campuri[0]);
+            String prenume = campuri[1];
+            String nume = campuri[2];
+            String grupa = campuri[3];
+
+            Student s = new Student(id, nume, prenume, grupa);
+            studenti.add(s);
+        }
+
+        // afisam studentii cititi
+        System.out.println("Studentii cititi din fisier:");
+        for (Student s : studenti) {
+            System.out.println(s);
+        }
+
+        // sortam dupa nume
+        studenti.sort(Comparator.comparing(Student::getNume));
+
+        // pregatim liniile pentru fisierul de iesire
+        List<String> outLines = new ArrayList<>();
+        for (Student s : studenti) {
+            String linie = s.getNrMatricol() + "," + s.getPrenume() + "," + s.getNume() + "," + s.getFormatieDeStudiu();
+            outLines.add(linie);
+        }
+
+        // salvam in studenti_out.txt
+        Files.write(Paths.get("src/studenti_out.txt"), outLines);
+
+        System.out.println("Lista sortata a fost salvata in studenti_out.txt");
+
+    } catch (IOException e) {
+        System.out.println("Gresire la scriere");
+    }
 }
